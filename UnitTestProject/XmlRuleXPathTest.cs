4a19f9ce4b6788a6cee6b2455aef8382000557b5
LAVN <rairairakurakulilac@hotmail.co.jp>
2015-03-30 00:34:15 +0900

    XmlRuleXPathクラス追加

---
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using XmlChecker;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class XmlRuleXPathTest
+	{
+		[TestMethod]
+		public void XA0101_GridTest()
+		{
+			var rule = new XmlRuleXPath(new string[] { "XA0101", "Error", "//*[name(.)='Grid'][not(*[2])]", "子要素を複数持たないGridは入れ子を解除してください。", });
+			var xaml = @"<Grid><TextBlock Text=""Test"" /></Grid>";
+			var xDoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = xDoc.XPathSelectElements(rule.XPath).ToList();
+			Assert.AreEqual(1, errorInstances.Count);
+		}
+
+		[TestMethod]
+		public void XA0102_GridTest()
+		{
+			var rule = new XmlRuleXPath(new string[] { "XA0102", "Error", "//*[name(.)='Grid']", "子要素を複数持たないGridは入れ子を解除してください。", });
+			var xaml = @"<Grid><TextBlock Text=""Test"" /></Grid>";
+			var xDoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = xDoc.XPathSelectElements(rule.XPath).ToList();
+			Assert.AreEqual(1, errorInstances.Count);
+		}
+
+		[TestMethod]
+		public void XA0201_StackPanelTest()
+		{
+			var rule = new XmlRuleXPath(new string[] { "XA0201", "Error", "//*[name(.)='StackPanel'][not(*[2])]", "子要素を複数持たないStackPanelは入れ子を解除してください。", });
+			var xaml = @"<StackPanel><TextBlock Text=""Test"" /></StackPanel>";
+			var xDoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = xDoc.XPathSelectElements(rule.XPath).ToList();
+			Assert.AreEqual(1, errorInstances.Count);
+		}
+
+		[TestMethod]
+		public void XA0301_BorderTest1()
+		{
+			var rule = new XmlRuleXPath(new string[] { "XA0301", "Error", "//*[name(.)='Border'][not(@Background)][not(@BorderBrush) or not(@BorderThickness)]", "罫線色を持たないBorderは表示されないので入れ子を解除してください。", });
+			var xaml = @"<Border Background=""Black""><TextBlock Text=""Test"" /></Border>";
+			var xDoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = xDoc.XPathSelectElements(rule.XPath).ToList();
+			Assert.AreEqual(0, errorInstances.Count);
+		}
+
+		[TestMethod]
+		public void XA0301_BorderTest2()
+		{
+			var rule = new XmlRuleXPath(new string[] { "XA0301", "Error", "//*[name(.)='Border'][not(@Background)][not(@BorderBrush) or not(@BorderThickness)]", "背景色も罫線を持たないBorderは表示されないので入れ子を解除してください。", });
+			var xaml = @"<Border><TextBlock Text=""Test"" /></Border>";
+			var xDoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = xDoc.XPathSelectElements(rule.XPath).ToList();
+			Assert.AreEqual(1, errorInstances.Count);
+		}
+
+		[TestMethod]
+		public void XA0301_BorderTest3()
+		{
+			var rule = new XmlRuleXPath(new string[] { "XA0301", "Error", "//*[name(.)='Border'][not(@Background)][not(@BorderBrush) or not(@BorderThickness)]", "背景色も罫線を持たないBorderは表示されないので入れ子を解除してください。", });
+			var xaml = @"<Border BorderThickness=""1""><TextBlock Text=""Test"" /></Border>";
+			var xDoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = xDoc.XPathSelectElements(rule.XPath).ToList();
+			Assert.AreEqual(1, errorInstances.Count);
+		}
+
+		[TestMethod]
+		public void XA0301_BorderTest4()
+		{
+			var rule = new XmlRuleXPath(new string[] { "XA0301", "Error", "//*[name(.)='Border'][not(@BorderBrush) or not(@BorderThickness)]", "背景色も罫線を持たないBorderは表示されないので入れ子を解除してください。", });
+			var xaml = @"<Border BorderBrush=""Black""><TextBlock Text=""Test"" /></Border>";
+			var xDoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = xDoc.XPathSelectElements(rule.XPath).ToList();
+			Assert.AreEqual(1, errorInstances.Count);
+		}
+
+		[TestMethod]
+		public void XA0401_ScrollViewerTest()
+		{
+			var rule = new XmlRuleXPath(new string[] { "XA0401", "Error", "//*[name(.)='ScrollViewer'][not(@Background)]", "マウスホイールに反応させるため、ScrollViewrにはBackgroundを指定してください。", });
+			var xaml = @"<ScrollViewer><TextBlock Text=""Test"" /></ScrollViewer>";
+			var xDoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = xDoc.XPathSelectElements(rule.XPath).ToList();
+			Assert.AreEqual(1, errorInstances.Count);
+		}
+
+		[TestMethod]
+		public void XA0501_TextBlockTest()
+		{
+			var rule = new XmlRuleXPath(new string[] { "XA0501", "Error", "//*[name(.)='TextBlock'][contains(@Text,'TwoWay')]", "TextBlock.TextにはTwoWayバインドを指定しないでください。", });
+			var xaml = @"<TextBlock Text=""{Binding Text, Mode=TwoWay}"" />";
+			var xDoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = xDoc.XPathSelectElements(rule.XPath).ToList();
+			Assert.AreEqual(1, errorInstances.Count);
+		}
+
+		[TestMethod]
+		public void XA0601_TextBoxTest()
+		{
+			var rule = new XmlRuleXPath(new string[] { "XA0601", "Error", "//*[name(.)='TextBox'][contains(@Text,'{Binding')][not(contains(@Text,'TwoWay'))]", "TextBox.TextのバインドがTwoWayバインドされていません。", });
+			var xaml = @"<TextBox Text=""{Binding Text}"" />";
+			var xDoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = xDoc.XPathSelectElements(rule.XPath).ToList();
+			Assert.AreEqual(1, errorInstances.Count);
+		}
+	}
+}
---
+++ XmlChecker/XmlRuleXPath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace XmlChecker
+{
+	public class XmlRuleXPath
+	{
+		public string Id { get; private set; }
+
+		public string Level { get; private set; }
+
+		public string XPath { get; private set; }
+
+		public string Message { get; private set; }
+
+		public XmlRuleXPath(string[] line)
+		{
+			if (line == null || line.Length != 4)
+			{
+				throw new Exception();
+			}
+
+			this.Id = line[0];
+			this.Level = line[1];
+			this.XPath = line[2];
+			this.Message = line[3];
+		}
+	}
+}
