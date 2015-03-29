f443f8b0ad60e7f47c779213d6bcaae2a73b9073
LAVN <rairairakurakulilac@hotmail.co.jp>
2015-03-30 00:33:12 +0900

    CsvParserクラス追加

---
+++ UnitTestProject/CsvParserTest.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XmlChecker;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class CsvUnitTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			string source = "test1,test2,test3";
+			var result = CsvParser.Parse(source);
+
+			Assert.AreEqual(1, result.Count, "レコード数が想定外です");
+			Assert.AreEqual(3, result[0].Length, "フィールド数1が想定外です");
+			Assert.AreEqual("test1", result[0][0], "フィールド1-1が想定外です");
+			Assert.AreEqual("test2", result[0][1], "フィールド1-2が想定外です");
+			Assert.AreEqual("test3", result[0][2], "フィールド1-3が想定外です");
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			string source = "test1,test2,test3\r\n" + "test1,test2,test3";
+			var result = CsvParser.Parse(source);
+
+			Assert.AreEqual(2, result.Count, "レコード数が想定外です");
+
+			Assert.AreEqual(3, result[0].Length, "フィールド数1が想定外です");
+			Assert.AreEqual("test1", result[0][0], "フィールド1-1が想定外です");
+			Assert.AreEqual("test2", result[0][1], "フィールド1-2が想定外です");
+			Assert.AreEqual("test3", result[0][2], "フィールド1-3が想定外です");
+
+			Assert.AreEqual(3, result[1].Length, "フィールド数2が想定外です");
+			Assert.AreEqual("test1", result[1][0], "フィールド2-1が想定外です");
+			Assert.AreEqual("test2", result[1][1], "フィールド2-2が想定外です");
+			Assert.AreEqual("test3", result[1][2], "フィールド2-3が想定外です");
+		}
+
+		[TestMethod]
+		public void TestMethod3()
+		{
+			string source = "test1,test2,test3\n" + "test1,test2,test3";
+			var result = CsvParser.Parse(source);
+
+			Assert.AreEqual(2, result.Count, "レコード数が想定外です");
+
+			Assert.AreEqual(3, result[0].Length, "フィールド数1が想定外です");
+			Assert.AreEqual("test1", result[0][0], "フィールド1-1が想定外です");
+			Assert.AreEqual("test2", result[0][1], "フィールド1-2が想定外です");
+			Assert.AreEqual("test3", result[0][2], "フィールド1-3が想定外です");
+
+			Assert.AreEqual(3, result[1].Length, "フィールド数2が想定外です");
+			Assert.AreEqual("test1", result[1][0], "フィールド2-1が想定外です");
+			Assert.AreEqual("test2", result[1][1], "フィールド2-2が想定外です");
+			Assert.AreEqual("test3", result[1][2], "フィールド2-3が想定外です");
+		}
+
+		[TestMethod]
+		public void TestMethod4()
+		{
+			string source = "test1,test2,test3\r" + "test4,test5,test6";
+			var result = CsvParser.Parse(source);
+
+			Assert.AreEqual(2, result.Count, "レコード数が想定外です");
+
+			Assert.AreEqual(3, result[0].Length, "フィールド数1が想定外です");
+			Assert.AreEqual(3, result[1].Length, "フィールド数1が想定外です");
+			Assert.AreEqual("test1", result[0][0], "フィールド1-1が想定外です");
+			Assert.AreEqual("test2", result[0][1], "フィールド1-2が想定外です");
+			Assert.AreEqual("test3", result[0][2], "フィールド1-3が想定外です");
+
+			Assert.AreEqual("test4", result[1][0], "フィールド1-4が想定外です");
+			Assert.AreEqual("test5", result[1][1], "フィールド1-5が想定外です");
+			Assert.AreEqual("test6", result[1][2], "フィールド1-6が想定外です");
+		}
+	}
+}
---
+++ XmlChecker/CsvParser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace XmlChecker
+{
+	public static class CsvParser
+	{
+		public static List<string[]> Parse(string ruleStr)
+		{
+			return ParseCore(ruleStr).ToList();
+		}
+
+		private static IEnumerable<string[]> ParseCore(string ruleStr)
+		{
+			using (var sr = new MemoryStream(Encoding.UTF8.GetBytes(ruleStr)))
+			using (var parser = new TextFieldParser(sr))
+			{
+				parser.SetDelimiters(","); // 区切り文字はコンマ
+
+				while (!parser.EndOfData)
+				{
+					string[] row = parser.ReadFields(); // 1行読み込み
+					yield return row;
+				}
+			}
+		}
+	}
+}
