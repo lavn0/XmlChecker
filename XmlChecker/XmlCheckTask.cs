6c1887798a9523a1eb6d0460ef87e644795f7299
LAVN <rairairakurakulilac@hotmail.co.jp>
2015-08-23 12:33:35 +0900

    XmlCheckerをビルドタスク化

---
+++ XmlChecker/XmlCheckTask.cs
@@ -0,0 +1,87 @@
+﻿namespace XmlChecker
+{
+	using Microsoft.Build.Utilities;
+	using System;
+	using System.IO;
+	using System.Linq;
+	using System.Text;
+
+	public class XmlCheckTask : Task
+	{
+		/// <summary>MSBuildプロセスで検証対象のファイル名を渡す</summary>
+		public string[] TargetFiles { get; set; }
+
+		/// <summary>MSBuildプロセスでCSVルールファイル名を渡す</summary>
+		public string[] RuleFiles { get; set; }
+
+		/// <summary>Error扱いとするレベル（その他はすべてWarning）</summary>
+		public string[] ErrorLevels { get; set; }
+
+		public override bool Execute()
+		{
+			if (RuleFiles == null)
+			{
+				Log.LogError("RuleFiles が指定されていません。");
+				return false;
+			}
+
+			var notExistRuleFile = RuleFiles.Where(f => !File.Exists(f)).ToList();
+			if (notExistRuleFile.Any())
+			{
+				Log.LogError("次の RuleFile が見つかりませんでした。" + string.Join(",", notExistRuleFile));
+				return false;
+			}
+
+			if (TargetFiles == null)
+			{
+				Log.LogError("TargetFiles が指定されていません。");
+				return false;
+			}
+
+			var notExistFiles = TargetFiles.Where(f => !File.Exists(f)).ToList();
+			if (notExistFiles.Any())
+			{
+				Log.LogError("次の TargetFile が見つかりませんでした。" + string.Join(",", notExistFiles));
+				return false;
+			}
+
+			var ruleStr = string.Join(Environment.NewLine, RuleFiles.Select(f => File.ReadAllText(f, Encoding.GetEncoding("SHIFT_JIS"))));
+			var ruleCsv = CsvParser.Parse(ruleStr).ToList();
+			var rules = ruleCsv.Select(l => new XmlRuleXPath(l)).ToList();
+			var violations = ViolationUtility.GetVioltations(TargetFiles, rules);
+
+			foreach (var rule in rules.Where(r => !r.IsValid.GetValueOrDefault(true)))
+			{
+				Log.LogError("ルールID={0} が異常終了しました。{1}", rule.Id, rule.XPathErrorMessage);
+				return false;
+			}
+
+			var groupedViolations = violations
+				.OrderBy(r => r.Level)
+				.ThenBy(r => r.FileName)
+				.ThenBy(r => r.LineNumber)
+				.ThenBy(r => r.LinePosition)
+				.ThenBy(r => r.ErrorCode)
+				.GroupBy(r => r.Level);
+
+			foreach (var item in groupedViolations)
+			{
+				foreach (var result in item)
+				{
+					var message = string.Format("{0}({1},{2}): {3} {4} {5}", result.FileName, result.LineNumber, result.LinePosition, result.ErrorCode, result.Level, result.Message);
+
+					if (ErrorLevels.Contains(result.Level))
+					{
+						Log.LogWarning(result.Level, result.ErrorCode, "ヘルプキーワード", result.FileName, result.LineNumber, result.LinePosition, result.LineNumber, result.LinePosition, result.Message);
+					}
+					else
+					{
+						Log.LogError(result.Level, result.ErrorCode, "ヘルプキーワード", result.FileName, result.LineNumber, result.LinePosition, result.LineNumber, result.LinePosition, result.Message);
+					}
+				}
+			}
+
+			return true;
+		}
+	}
+}
