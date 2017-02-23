f7a8b01cedeeee41dd6822c266a5b935357d56b7
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-02-23 23:07:07 +0900

    ユニットテストのリソースファイル追加 (XA0403)

--- UnitTestProject/XmlRuleXPathTest.cs
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -1383,16 +1383,26 @@ namespace UnitTestProject
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0403_OK.xaml", "Resources")]
 		public void XA0403_OK()
 		{
-			Assert.Inconclusive("ルール(ID=XA0403)の正常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0403", "Default", @"//*[name(.)='ScrollViewer']/@*[name(.)='HorizontalAlignment'][.='Stretch']", @"HorizontalAlignment=""Stretch""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0403_OK.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(0, errorInstances.Count);
 		}
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0403_NG1.xaml", "Resources")]
 		public void XA0403_NG()
 		{
-			Assert.Inconclusive("ルール(ID=XA0403)の異常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0403", "Default", @"//*[name(.)='ScrollViewer']/@*[name(.)='HorizontalAlignment'][.='Stretch']", @"HorizontalAlignment=""Stretch""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0403_NG1.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(1, errorInstances.Count);
 		}
 
 		[TestMethod]
