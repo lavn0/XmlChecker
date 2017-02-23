eac377686f3989887b5ac9e473fd0f3e4e456ff9
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-02-23 23:54:11 +0900

    ユニットテストのリソースファイル追加 (XA0410)

--- UnitTestProject/XmlRuleXPathTest.cs
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -1551,16 +1551,26 @@ namespace UnitTestProject
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0410_OK.xaml", "Resources")]
 		public void XA0410_OK()
 		{
-			Assert.Inconclusive("ルール(ID=XA0410)の正常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0410", "Default", @"//*[name(.)='ScrollViewer']/@*[name(.)='HorizontalScrollBarVisibility'][.='Auto']", @"ScrollViewer.HorizontalScrollBarVisibility=""Auto""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0410_OK.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(0, errorInstances.Count);
 		}
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0410_NG1.xaml", "Resources")]
 		public void XA0410_NG()
 		{
-			Assert.Inconclusive("ルール(ID=XA0410)の異常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0410", "Default", @"//*[name(.)='ScrollViewer']/@*[name(.)='HorizontalScrollBarVisibility'][.='Auto']", @"ScrollViewer.HorizontalScrollBarVisibility=""Auto""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0410_NG1.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(1, errorInstances.Count);
 		}
 
 		[TestMethod]
