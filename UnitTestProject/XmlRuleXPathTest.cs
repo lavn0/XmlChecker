09402bcb1f58c411be596a4caeda36c92ed02140
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-02-26 01:13:58 +0900

    ユニットテストのリソースファイル追加 (XA0801)

--- UnitTestProject/XmlRuleXPathTest.cs
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -2547,16 +2547,26 @@ namespace UnitTestProject
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0801_OK.xaml", "Resources")]
 		public void XA0801_OK()
 		{
-			Assert.Inconclusive("ルール(ID=XA0801)の正常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0801", "Default", @"//*[name(.)='Button']/@Visibility[.='Visible']", @"Button.Visibility=""Visible""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0801_OK.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(0, errorInstances.Count);
 		}
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0801_NG1.xaml", "Resources")]
 		public void XA0801_NG()
 		{
-			Assert.Inconclusive("ルール(ID=XA0801)の異常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0801", "Default", @"//*[name(.)='Button']/@Visibility[.='Visible']", @"Button.Visibility=""Visible""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0801_NG1.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(1, errorInstances.Count);
 		}
 
 		[TestMethod]
