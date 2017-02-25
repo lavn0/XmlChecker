282c9de9d76c5b1d748e1975cc6c9e1654d3cde6
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-02-25 13:51:40 +0900

    ユニットテストのリソースファイル追加 (XA0508)

--- UnitTestProject/XmlRuleXPathTest.cs
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -1863,16 +1863,26 @@ namespace UnitTestProject
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0508_OK.xaml", "Resources")]
 		public void XA0508_OK()
 		{
-			Assert.Inconclusive("ルール(ID=XA0508)の正常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0508", "Critical", @"//*[name(.)='TextBlock']/@Text[contains(.,'TwoWay')]", @"TextBlock.Text属性はTwoWayバインディングしないでください。", });
+			var xaml = File.ReadAllText(@"Resources\XA0508_OK.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(0, errorInstances.Count);
 		}
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0508_NG1.xaml", "Resources")]
 		public void XA0508_NG()
 		{
-			Assert.Inconclusive("ルール(ID=XA0508)の異常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0508", "Critical", @"//*[name(.)='TextBlock']/@Text[contains(.,'TwoWay')]", @"TextBlock.Text属性はTwoWayバインディングしないでください。", });
+			var xaml = File.ReadAllText(@"Resources\XA0508_NG1.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(1, errorInstances.Count);
 		}
 
 		[TestMethod]
