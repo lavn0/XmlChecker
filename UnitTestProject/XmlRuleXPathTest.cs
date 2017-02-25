882fb65e3c6e97897a683d53a5ecbdf37fb51b5c
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-02-26 00:33:23 +0900

    ユニットテストのリソースファイル追加 (XA0706)

--- UnitTestProject/XmlRuleXPathTest.cs
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -2331,16 +2331,26 @@ namespace UnitTestProject
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0706_OK.xaml", "Resources")]
 		public void XA0706_OK()
 		{
-			Assert.Inconclusive("ルール(ID=XA0706)の正常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0706", "Default", @"//*[name(.)='CheckBox']/@VerticalContentAlignment[.='Top']", @"CheckBox.VerticalContentAlignment=""Top""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0706_OK.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(0, errorInstances.Count);
 		}
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0706_NG1.xaml", "Resources")]
 		public void XA0706_NG()
 		{
-			Assert.Inconclusive("ルール(ID=XA0706)の異常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0706", "Default", @"//*[name(.)='CheckBox']/@VerticalContentAlignment[.='Top']", @"CheckBox.VerticalContentAlignment=""Top""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0706_NG1.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(1, errorInstances.Count);
 		}
 
 		[TestMethod]
