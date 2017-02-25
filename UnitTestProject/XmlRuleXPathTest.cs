fe156ca7308e71e631b0ae625a85ebbffdd5809d
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-02-26 00:20:14 +0900

    ユニットテストのリソースファイル追加 (XA0701)

--- UnitTestProject/XmlRuleXPathTest.cs
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -2211,16 +2211,26 @@ namespace UnitTestProject
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0701_OK.xaml", "Resources")]
 		public void XA0701_OK()
 		{
-			Assert.Inconclusive("ルール(ID=XA0701)の正常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0701", "Default", @"//*[name(.)='CheckBox']/@Visibility[.='Visible']", @"CheckBox.Visibility=""Visible""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0701_OK.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(0, errorInstances.Count);
 		}
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0701_NG1.xaml", "Resources")]
 		public void XA0701_NG()
 		{
-			Assert.Inconclusive("ルール(ID=XA0701)の異常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0701", "Default", @"//*[name(.)='CheckBox']/@Visibility[.='Visible']", @"CheckBox.Visibility=""Visible""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0701_NG1.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(1, errorInstances.Count);
 		}
 
 		[TestMethod]
