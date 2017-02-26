ff91cbdfe69c75a955877247ec259e028cb11180
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-02-26 20:16:22 +0900

    ユニットテストのリソースファイル追加 (XA1102)

--- UnitTestProject/XmlRuleXPathTest.cs
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -3363,16 +3363,26 @@ namespace UnitTestProject
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA1102_OK.xaml", "Resources")]
 		public void XA1102_OK()
 		{
-			Assert.Inconclusive("ルール(ID=XA1102)の正常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA1102", "Default", @"//*[name(.)='ComboBox']/@IsEnabled[.='True']", @"ComboBox.IsEnabled=""True""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA1102_OK.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(0, errorInstances.Count);
 		}
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA1102_NG1.xaml", "Resources")]
 		public void XA1102_NG()
 		{
-			Assert.Inconclusive("ルール(ID=XA1102)の異常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA1102", "Default", @"//*[name(.)='ComboBox']/@IsEnabled[.='True']", @"ComboBox.IsEnabled=""True""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA1102_NG1.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(1, errorInstances.Count);
 		}
 
 		[TestMethod]
