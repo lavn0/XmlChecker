e233321ebfa756ea77172c3734cf1351c70c21a1
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-02-26 20:20:55 +0900

    ユニットテストのリソースファイル追加 (XA1105)

--- UnitTestProject/XmlRuleXPathTest.cs
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -3435,16 +3435,26 @@ namespace UnitTestProject
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA1105_OK.xaml", "Resources")]
 		public void XA1105_OK()
 		{
-			Assert.Inconclusive("ルール(ID=XA1105)の正常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA1105", "Default", @"//*[name(.)='ComboBox']/@HorizontalContentAlignment[.='Left']", @"ComboBox.HorizontalContentAlignment=""Left""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA1105_OK.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(0, errorInstances.Count);
 		}
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA1105_NG1.xaml", "Resources")]
 		public void XA1105_NG()
 		{
-			Assert.Inconclusive("ルール(ID=XA1105)の異常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA1105", "Default", @"//*[name(.)='ComboBox']/@HorizontalContentAlignment[.='Left']", @"ComboBox.HorizontalContentAlignment=""Left""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA1105_NG1.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(1, errorInstances.Count);
 		}
 
 		[TestMethod]
