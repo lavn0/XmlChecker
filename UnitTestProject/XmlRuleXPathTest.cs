922b3b91da41f38f88224c285a25557042907cd4
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-02-26 20:24:01 +0900

    ユニットテストのリソースファイル追加 (XA1107)

--- UnitTestProject/XmlRuleXPathTest.cs
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -3483,16 +3483,26 @@ namespace UnitTestProject
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA1107_OK.xaml", "Resources")]
 		public void XA1107_OK()
 		{
-			Assert.Inconclusive("ルール(ID=XA1107)の正常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA1107", "Default", @"//*[name(.)='ComboBox'][not(@Name)]/@Margin[.='0' or .='0,0' or .='0 0' or .='0,0,0,0' or .='0 0 0 0']", @"ComboBox.Margin=""0""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA1107_OK.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(0, errorInstances.Count);
 		}
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA1107_NG5.xaml", "Resources")]
 		public void XA1107_NG()
 		{
-			Assert.Inconclusive("ルール(ID=XA1107)の異常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA1107", "Default", @"//*[name(.)='ComboBox'][not(@Name)]/@Margin[.='0' or .='0,0' or .='0 0' or .='0,0,0,0' or .='0 0 0 0']", @"ComboBox.Margin=""0""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA1107_NG5.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(5, errorInstances.Count);
 		}
 
 		[TestMethod]
