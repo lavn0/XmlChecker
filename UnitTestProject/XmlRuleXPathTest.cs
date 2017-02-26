e2820bc07b663aaa2ce687f96f6b76f55fbe7bf2
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-02-26 12:35:15 +0900

    ユニットテストのリソースファイル追加 (XA0905)

--- UnitTestProject/XmlRuleXPathTest.cs
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -2883,16 +2883,26 @@ namespace UnitTestProject
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0905_OK.xaml", "Resources")]
 		public void XA0905_OK()
 		{
-			Assert.Inconclusive("ルール(ID=XA0905)の正常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0905", "Default", @"//*[name(.)='HyperlinkButton']/@HorizontalContentAlignment[.='Left']", @"HyperlinkButton.HorizontalContentAlignment=""Left""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0905_OK.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(0, errorInstances.Count);
 		}
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0905_NG1.xaml", "Resources")]
 		public void XA0905_NG()
 		{
-			Assert.Inconclusive("ルール(ID=XA0905)の異常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0905", "Default", @"//*[name(.)='HyperlinkButton']/@HorizontalContentAlignment[.='Left']", @"HyperlinkButton.HorizontalContentAlignment=""Left""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0905_NG1.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(1, errorInstances.Count);
 		}
 
 		[TestMethod]
