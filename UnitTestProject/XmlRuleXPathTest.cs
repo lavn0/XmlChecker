ba9ad23aa1c7b2fa371c83240f51d46b943b3da5
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-02-26 12:41:40 +0900

    ユニットテストのリソースファイル追加 (XA0907)

--- UnitTestProject/XmlRuleXPathTest.cs
+++ UnitTestProject/XmlRuleXPathTest.cs
@@ -2931,16 +2931,26 @@ namespace UnitTestProject
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0907_OK.xaml", "Resources")]
 		public void XA0907_OK()
 		{
-			Assert.Inconclusive("ルール(ID=XA0907)の正常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0907", "Default", @"//*[name(.)='HyperlinkButton'][not(@Name)]/@Margin[.='0' or .='0,0' or .='0 0' or .='0,0,0,0' or .='0 0 0 0']", @"HyperlinkButton.Margin=""0""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0907_OK.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(0, errorInstances.Count);
 		}
 
 		[TestMethod]
 		[TestCategory("XmlChecker")]
+		[DeploymentItem(@"Resources\XA0907_NG5.xaml", "Resources")]
 		public void XA0907_NG()
 		{
-			Assert.Inconclusive("ルール(ID=XA0907)の異常系テストが作成されていません。");
+			var rule = new XmlRuleXPath(string.Empty, 0, new string[] { "XA0907", "Default", @"//*[name(.)='HyperlinkButton'][not(@Name)]/@Margin[.='0' or .='0,0' or .='0 0' or .='0,0,0,0' or .='0 0 0 0']", @"HyperlinkButton.Margin=""0""はデフォルト値です。", });
+			var xaml = File.ReadAllText(@"Resources\XA0907_NG5.xaml");
+			var xdoc = XDocument.Parse(xaml, LoadOptions.SetLineInfo);
+			var errorInstances = ((IEnumerable<object>)xdoc.XPathEvaluate(rule.XPath)).Cast<XObject>().ToList();
+			Assert.AreEqual(5, errorInstances.Count);
 		}
 
 		[TestMethod]
